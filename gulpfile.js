0bdd2c49067246bf1789e450c4224f7afbf04e0d
Denis Syroezhkin <kol9743@gmail.com>
2016-08-18 00:40:13 +0300

    beatiful formatter

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp         = require('gulp');
-var plumber      = require('gulp-plumber');
-var rename       = require('gulp-rename');
-var concat       = require('gulp-concat');
-var uglify       = require('gulp-uglify');
-var cssmin       = require('gulp-cssmin');
-var imagemin     = require('gulp-imagemin');
-var sass         = require('gulp-sass');
-var csslint      = require('gulp-csslint'); //csslint
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var csslint = require('gulp-csslint'); //csslint
 
-var postcss      = require('gulp-postcss'); //PostCSS
-var csswring     = require('csswring');
+var postcss = require('gulp-postcss'); //PostCSS
+var csswring = require('csswring');
 var autoprefixer = require('autoprefixer'); //Autoprefixer
-var cssnext      = require('cssnext');
-var precss       = require('precss');
+var cssnext = require('cssnext');
+var precss = require('precss');
 
 //Based project directory
 var baseDir = "./public";
@@ -24,49 +24,55 @@ gulp.task('images', function() {
 });
 
 gulp.task('csslint', function() {
-  return gulp.src(baseDir + '/css/style.css') 
+  return gulp.src(baseDir + '/css/style.css')
     .pipe(csslint('csslintrc.json'))
     .pipe(csslint.reporter());
 });
 
-gulp.task('postcss', ['csslint'], function(){
+gulp.task('postcss', ['csslint'], function() {
   var processors = [
-    autoprefixer 
+    autoprefixer
   ];
   return gulp.src(baseDir + '/css/*.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest(baseDir + '/css/'))
 });
 
-gulp.task('styles', function(){
+gulp.task('styles', function() {
   gulp.src([baseDir + '/sass/**/*.sass'])
     .pipe(plumber({
-      errorHandler: function (error) {
+      errorHandler: function(error) {
         console.log(error.message);
         this.emit('end');
-    }}))
-    
-    .pipe(sass({outputStyle: 'expanded'}))
+      }
+    }))
+
+  .pipe(sass({
+      outputStyle: 'expanded'
+    }))
     .pipe(gulp.dest(baseDir + '/css/'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(cssmin())
     .pipe(gulp.dest(baseDir + '/css/'))
 });
 
-gulp.task('scripts', function(){
+gulp.task('scripts', function() {
   return gulp.src(baseDir + '/js/libs/**/*.js')
     .pipe(plumber({
-      errorHandler: function (error) {
+      errorHandler: function(error) {
         console.log(error.message);
         this.emit('end');
-    }}))
+      }
+    }))
     .pipe(concat('libs.js'))
     .pipe(gulp.dest(baseDir + '/js/'))
     .pipe(uglify())
     .pipe(gulp.dest(baseDir + '/js/'))
-}); 
+});
 
-gulp.task('default', function(){
+gulp.task('default', function() {
   gulp.watch(baseDir + "/sass/**/*.sass", ['styles']);
   gulp.watch(baseDir + "/css/**/*.css", ['postcss']);
   gulp.watch(baseDir + "/js/libs/**/*.js", ['scripts']);
--- routes.js
+++ routes.js
@@ -12,168 +12,176 @@ var logger = log4js.getLogger();
 // Export a function, so that we can pass 
 // the app and io instances from the app.js file:
 
-module.exports = function(app,io){
-	
-
-	app.get('/', function(req, res){
-		logger.info(req.connection.remoteAddress, req.method, req.url, req.headers['user-agent']);
-
-		// Render views/home.html
-		res.render('home');
-	});
-
-	app.get('/create', function(req,res){
-		
-		// Generate unique id for the room
-		var id = Math.round((Math.random() * 1000000));
-
-		// Redirect to the random room
-		res.redirect('/chat/'+id);
-	});
-
-	app.get('/chat/:id', function(req,res){
-		logger.info(req.connection.remoteAddress, req.method, req.url, req.headers['user-agent']);
-		// Render the chant.html view
-		res.render('chat');
-	});
-
-	app.get('/feedback', function(req,res){
-		
-		// Redirect to the discussion page
-		res.render('feedback');
-	});
-
-	// Initialize a new socket.io application, named 'chat'
-	var chat = io.on('connection', function (socket) {
-
-		// When the client emits the 'load' event, reply with the 
-		// number of people in this chat room
-
-		socket.on('load',function(data){
-
-			var room = findClientsSocket(io,data);
-			if(room.length === 0 ) {
-
-				socket.emit('peopleinchat', {number: 0});
-			}
-			else if(room.length === 1) {
-
-				socket.emit('peopleinchat', {
-					number: 1,
-					user: room[0].username,
-					avatar: room[0].avatar,
-					id: data
-				});
-			}
-			else if(room.length >= 2) {
-
-				chat.emit('tooMany', {boolean: true});
-			}
-		});
-
-		// When the client emits 'login', save his name and avatar,
-		// and add them to the room
-		socket.on('login', function(data) {
-			
-			var room = findClientsSocket(io, data.id);
-			// Only two people per room are allowed
-			if (room.length < 2) {
-
-				// Use the socket object to store data. Each client gets
-				// their own unique socket object
-				socket.lang = data.lang;
-				socket.username = data.user;
-				socket.room = data.id;
-				socket.avatar = gravatar.url(data.avatar, {s: '140', r: 'x', d: 'mm'});
-
-				// Tell the person what he should use for an avatar
-				socket.emit('img', socket.avatar);
+module.exports = function(app, io) {
 
 
-				// Add the client to the room
-				socket.join(data.id);
+  app.get('/', function(req, res) {
+    logger.info(req.connection.remoteAddress, req.method, req.url, req.headers['user-agent']);
 
-				if (room.length == 1) {
+    // Render views/home.html
+    res.render('home');
+  });
 
-					var usernames = [],
-							langs = [],
-						avatars = [];
+  app.get('/create', function(req, res) {
 
-					usernames.push(room[0].username);
-					usernames.push(socket.username);
+    // Generate unique id for the room
+    var id = Math.round((Math.random() * 1000000));
 
-					langs.push(room[0].lang);
-					langs.push(socket.lang);
+    // Redirect to the random room
+    res.redirect('/chat/' + id);
+  });
 
-					avatars.push(room[0].avatar);
-					avatars.push(socket.avatar);
+  app.get('/chat/:id', function(req, res) {
+    logger.info(req.connection.remoteAddress, req.method, req.url, req.headers['user-agent']);
+    // Render the chant.html view
+    res.render('chat');
+  });
 
-					// Send the startChat event to all the people in the
-					// room, along with a list of people that are in it.
+  app.get('/feedback', function(req, res) {
 
-					chat.in(data.id).emit('startChat', {
-						boolean: true,
-						id: data.id,
-						users: usernames,
-						langs: langs,
-						avatars: avatars
-					});
-				}
-			}
-			else {
-				socket.emit('tooMany', {boolean: true});
-			}
+    // Redirect to the discussion page
+    res.render('feedback');
+  });
 
-			//Logs on terminal
-			console.log(socket.room + " + " + socket.username + " + " + socket.lang); 
-			
-		});
+  // Initialize a new socket.io application, named 'chat'
+  var chat = io.on('connection', function(socket) {
 
-		// Somebody left the chat
-		socket.on('disconnect', function() {
+    // When the client emits the 'load' event, reply with the 
+    // number of people in this chat room
 
-			// Notify the other person in the chat room
-			// that his partner has left
+    socket.on('load', function(data) {
 
-			socket.broadcast.to(this.room).emit('leave', {
-				boolean: true,
-				room: this.room,
-				user: this.username,
-				avatar: this.avatar
-			});
+      var room = findClientsSocket(io, data);
+      if (room.length === 0) {
 
-			// leave the room
-			socket.leave(socket.room);
-		});
+        socket.emit('peopleinchat', {
+          number: 0
+        });
+      } else if (room.length === 1) {
+
+        socket.emit('peopleinchat', {
+          number: 1,
+          user: room[0].username,
+          avatar: room[0].avatar,
+          id: data
+        });
+      } else if (room.length >= 2) {
+
+        chat.emit('tooMany', {
+          boolean: true
+        });
+      }
+    });
+
+    // When the client emits 'login', save his name and avatar,
+    // and add them to the room
+    socket.on('login', function(data) {
+
+      var room = findClientsSocket(io, data.id);
+      // Only two people per room are allowed
+      if (room.length < 2) {
+
+        // Use the socket object to store data. Each client gets
+        // their own unique socket object
+        socket.lang = data.lang;
+        socket.username = data.user;
+        socket.room = data.id;
+        socket.avatar = gravatar.url(data.avatar, {
+          s: '140',
+          r: 'x',
+          d: 'mm'
+        });
+
+        // Tell the person what he should use for an avatar
+        socket.emit('img', socket.avatar);
 
 
-		// Handle the sending of messages
-		socket.on('msg', function(data){
+        // Add the client to the room
+        socket.join(data.id);
 
-			// When the server receives a message, it sends it to the other person in the room.
-			socket.broadcast.to(socket.room).emit('receive', {msg: data.msg, user: data.user, img: data.img});
-		});
-	});
+        if (room.length == 1) {
+
+          var usernames = [],
+            langs = [],
+            avatars = [];
+
+          usernames.push(room[0].username);
+          usernames.push(socket.username);
+
+          langs.push(room[0].lang);
+          langs.push(socket.lang);
+
+          avatars.push(room[0].avatar);
+          avatars.push(socket.avatar);
+
+          // Send the startChat event to all the people in the
+          // room, along with a list of people that are in it.
+
+          chat.in(data.id).emit('startChat', {
+            boolean: true,
+            id: data.id,
+            users: usernames,
+            langs: langs,
+            avatars: avatars
+          });
+        }
+      } else {
+        socket.emit('tooMany', {
+          boolean: true
+        });
+      }
+
+      //Logs on terminal
+      console.log(socket.room + " + " + socket.username + " + " + socket.lang);
+
+    });
+
+    // Somebody left the chat
+    socket.on('disconnect', function() {
+
+      // Notify the other person in the chat room
+      // that his partner has left
+
+      socket.broadcast.to(this.room).emit('leave', {
+        boolean: true,
+        room: this.room,
+        user: this.username,
+        avatar: this.avatar
+      });
+
+      // leave the room
+      socket.leave(socket.room);
+    });
+
+
+    // Handle the sending of messages
+    socket.on('msg', function(data) {
+
+      // When the server receives a message, it sends it to the other person in the room.
+      socket.broadcast.to(socket.room).emit('receive', {
+        msg: data.msg,
+        user: data.user,
+        img: data.img
+      });
+    });
+  });
 };
 
-function findClientsSocket(io,roomId, namespace) {
-	var res = [],
-		ns = io.of(namespace || "/");    // the default namespace is "/"
+function findClientsSocket(io, roomId, namespace) {
+  var res = [],
+    ns = io.of(namespace || "/"); // the default namespace is "/"
 
-	if (ns) {
-		for (var id in ns.connected) {
-			if(roomId) {
-				var index = ns.connected[id].rooms.indexOf(roomId) ;
-				if(index !== -1) {
-					res.push(ns.connected[id]);
-				}
-			}
-			else {
-				res.push(ns.connected[id]);
-			}
-		}
-	}
-	return res;
+  if (ns) {
+    for (var id in ns.connected) {
+      if (roomId) {
+        var index = ns.connected[id].rooms.indexOf(roomId);
+        if (index !== -1) {
+          res.push(ns.connected[id]);
+        }
+      } else {
+        res.push(ns.connected[id]);
+      }
+    }
+  }
+  return res;
 }
-
-
